54897c0e01f36d271b528b62a4a328c85da91958
stephaniedofitas <stephaniedofitas@4a73f0ba-03bc-26ed-5ec3-fd41fe5d7a7c>
2013-09-10 17:52:54 +0000

    fixed file type of testbs and made a rough program to print drugList. *problem:only prints out one cell of drugs

---
+++ testsoup/testbs.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("TABLE-1f7e1308-e6f5-4380-8d85-e2a22bdec9db-drugInteractions-0.txt"))
+
+#print soup.prettify()
+
+table = soup.find('table')
+ 
+rows = table.findAll('tr')
+for tr in rows:
+  cols = tr.findAll('td')
+  for td in cols:
+      text = ''.join(td.find(text=True))
+      drugList= text.split(", ")
+      
+      #print text+"\n ***line break**",
+
+
+drugList= '\n'.join(drugList)
+
+print(drugList)
