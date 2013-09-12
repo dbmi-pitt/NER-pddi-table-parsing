81fe217c5740a84ca7e655f3e0f08a459f1e7ec0
stephaniedofitas@gmail.com <stephaniedofitas@gmail.com@4a73f0ba-03bc-26ed-5ec3-fd41fe5d7a7c>
2013-09-12 16:18:15 +0000

    edited testbs

--- testsoup/testbs.py
+++ testsoup/testbs.py
@@ -6,9 +6,19 @@ soup = BeautifulSoup(open("TABLE-1f7e1308-e6f5-4380-8d85-e2a22bdec9db-drugIntera
 tablecols = len(soup.find('td'))
 print(tablecols)
 
+#tables1=soup.findChildren('tr')
+#findChildrem method looks more effective
+#print(tables1)
+
+tables1=soup.find_all('tr')
+print(tables1)
+#****
 table = soup.find('table')
  
 rows = table.findAll('tr')
+labels=table.findAll('content')
+print(labels)
+
 for tr in rows:
   cols = tr.findAll('td')
   for td in cols:
@@ -18,6 +28,7 @@ for tr in rows:
       drugList= '\n'.join(drugList)
 
       print("\n" + drugList)
+      
       #print text+"\n ***line break**",
 
 
