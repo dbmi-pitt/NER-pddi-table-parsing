14ba172c636a0d102dbae11b0edf43026b02ce32
Steven DeMarco <std30@pitt.edu>
2015-11-06 12:02:17 -0500

    Adds a list of tableIDs that contain at least 1 drug mention.

--- scripts/OrganizeHTMLTablesShared/Organize.py
+++ scripts/OrganizeHTMLTablesShared/Organize.py
@@ -20,8 +20,7 @@ def is_empty(any_structure):
         return True
 
 def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i:i+n]
 
 def generate():
@@ -34,7 +33,7 @@ def generate():
 
     ## TODO: fix the script that pulls tables from Linked SPLs so that
     ## encoding and "prettify" are done at the time of extraction.
-    soupTemp = BeautifulSoup(html)
+    soupTemp = BeautifulSoup(html, "html.parser")
 
     output = open(os.path.join(dir, "output.txt"), "w")
     output.write(soupTemp.prettify().encode("utf-8"))
@@ -61,17 +60,23 @@ def organize():
     colsTot = 0
     colsTotTemp = 0
     rowsTot = 0
-    dir = os.path.dirname(__file__)
+    dir = os.path.dirname(__file__)  # Absolute path of the directory where the program resides
 
-    input = open(os.path.join(dir, "output.txt"), "r")
-    htmlParse = input.read().decode("utf-8")
+    input = open(os.path.join(dir, "output.txt"), "r")  # Opens output.txt
+    htmlParse = input.read()  # Sets htmlParse to reading the input with utf-8 decoding
 
     # Section prepares each part for addition to tableInfo dictionary
-    soup = BeautifulSoup(htmlParse)
-    tables = soup.findChildren("table") # (ResultSet)
+    soup = BeautifulSoup(htmlParse, "html.parser")
+    tables = soup.findChildren("table")  # (ResultSet) - finds all the <table> tags in the HTML of output.txt
 
-    tableIDs = [(n["value"]) for n in soup.findChildren("input")] # Creates a list of table names (List)
+    # Creates a list of table names (List) -- using input tag to define table name which is stored in value attribute
+    tableIDs = [(n["value"]) for n in soup.findChildren("input")]
 
+    #############################################################################
+    ##  - Loops through each table and finds every row in each table.          ##
+    ##  - Gathers variables for statistical analysis: amount of cells per row: ##
+    ##    i.e. # of tables, # of columns, # of rows, unique header names.      ##
+    #############################################################################
     for c in range(0, len(tableIDs) - 1): # Goes through all tables
         tableNo = tables[c].findChildren(["tr"]) # Finds all rows in a table (ResultSet)
 
@@ -80,6 +85,7 @@ def organize():
             dataCellsCols = len(allTable) # The amount of cells per row (columns)
             colsTotTemp += dataCellsCols
 
+            print(tableNo.index(line))
             if (line.findChildren(["th", tableNo.index(line) == 0 and line.findChildren(["th"])])) or (tableNo.index(line) == 0 and line.findChildren(["td"])):
                 headers.append(line.findChildren(["th", "td"]))
 
@@ -95,14 +101,20 @@ def organize():
         tableStatsCols.append(dataCellsCols)
         tableStatsRows.append(len(tableNo))
 
-    for n in range (0, len(headers)):
-        for i in range (0, len(headers[n])):
+    ##########################################################
+    ##  - Extracts the text from all of the table headers.  ##
+    ##########################################################
+    for n in range(0, len(headers)):
+        for i in range(0, len(headers[n])):
             dataHTML = str(headers[n][i])
             soup = BeautifulSoup(dataHTML)
             tempData = (re.sub(' +',' ', soup.text.strip("\t\n\r").replace("\n", "").strip())).upper().encode("utf-8")
 
             headersText.append(tempData)
 
+    #######################################################
+    ##  - Loops through the number of columns per table. ##
+    #######################################################
     for t in range (0, len(perTableCol)):
         for n in range (0, perTableCol[t][0]):
             newNum = perTableCol[t][0]
@@ -112,38 +124,54 @@ def organize():
             elif newNum == 0:
                 None
 
+    ###############################################
+    ##  - Gathers content of each table's rows.  ##
+    ###############################################
     for t in range (0, len(perTableCol)):
         for i in range (0, len(perTableCol[t])):
             if perTableCol[t][i] > 0:
                 perTableColLong.append(perTableCol[t][i])
 
+    ###############################################################
+    ##  - Adds the number of columns per table to tablePosInfo.  ##
+    ###############################################################
     for t in range (0, len(perTableColLong)):
         tablePosInfo[t][2] = "Column: " + str(perTableColLong[t])
         tablePosInfo[t] = tuple(tablePosInfo[t])
 
+    #####################################################
+    ##  - Adds the table position info to each table,  ##
+    ##    identified by the header text.               ##
+    #####################################################
     for delvt, pin in zip(headersText, tablePosInfo):
         tableInfo[delvt].append(pin)
 
+    ##########################################################
+    ##  - Gathers all of the keys of the list of tableInfo  ##
+    ##  - Prints each of the keys on a new line             ##
+    ##########################################################
     stringy = ""
     for key in tableInfo.keys():
         key = re.sub('\t+', ' ', key)
         stringy = stringy + key + "\n"
 
-    print stringy
+    print(stringy)
 
     headersFile = open(os.path.join(dir, "headers.txt"), "w")
     headersFile.write(str(tableInfo))
 
-    print "Total Number of Columns: " + str(colsTot)
-    print "Minimum Number of Columns: " + str(min(tableStatsCols))
-    print "Maximum Number of Columns: " + str(max(tableStatsCols))
-    print "Average Number of Columns: " + str(sum(tableStatsCols)/len(tableStatsCols)) + "\n\n"
+    # Print summary statistics of the tables.
+    print("Total Number of Columns: " + str(colsTot))
+    print("Minimum Number of Columns: " + str(min(tableStatsCols)))
+    print("Maximum Number of Columns: " + str(max(tableStatsCols)))
+    print("Average Number of Columns: " + str(sum(tableStatsCols)/len(tableStatsCols)) + "\n\n")
 
-    print "Total Number of Rows: " + str(rowsTot)
-    print "Minimum Number of Rows: " + str(min(tableStatsRows))
-    print "Maximum Number of Rows: " + str(max(tableStatsRows))
-    print "Average Number of Rows: " + str(sum(tableStatsRows)/len(tableStatsRows)) + "\n\n"
+    print("Total Number of Rows: " + str(rowsTot))
+    print("Minimum Number of Rows: " + str(min(tableStatsRows)))
+    print("Maximum Number of Rows: " + str(max(tableStatsRows)))
+    print("Average Number of Rows: " + str(sum(tableStatsRows)/len(tableStatsRows)) + "\n\n")
 
+# Takes a text database and categorizes everything into a directory
 def dbToDict():
     connect = {}
     dir = os.path.dirname(__file__)
@@ -169,6 +197,7 @@ def dbToDict():
     file = open(os.path.join(dir, "dbToDict.txt"), "w")
     file.write(str(connect))
 
+# Gathers a list of all of the drug mentions in the tables.
 def drugMentions():
     dir = os.path.dirname(__file__)
     finalList = []
@@ -199,4 +228,4 @@ def drugMentions():
         file = open(os.path.join(dir, finalList[c][0].replace("TABLE-", "")), "w")
         file.write(str(finalList[c][1]))
 
-drugMentions()
+organize()
